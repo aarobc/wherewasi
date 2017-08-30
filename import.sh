00596e31d72121f439daf4f45216eef27ffa6ddd
aarobc <aaron.b.christiansen@gmail.com>
2017-08-29 23:35:46 -0600

    history file passed into import script

--- import.sh
+++ import.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -xe
 # starting docker stuff
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+    exit
+fi
 docker-compose up -d
 echo "copying file"
 file=bak.json
-cp history.json $file
+cp $1 $file
 echo "Replace header"
 perl -0777 -pi -e 's/\A[^\n]*\n[^\n]*\n/{\n/g' $file
 echo "Replace footer"
